2639c98d27b4626268990d9b98d158b37627c187
DuongNA <duongna@ltsgroup.tech>
2024-11-05 00:37:46 +0700

    Localization
    Localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:acb/screens/login/UI/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -12,11 +14,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      // Use these imports instead of the below one since The AppLocalizations class
+      // also provides auto-generated localizationsDelegates and supportedLocales lists
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      /*localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('vi'), // Vietnamese
+      ],*/
       home: const LoginScreen(),
     );
   }
--- lib/screens/login/UI/login.dart
+++ lib/screens/login/UI/login.dart
@@ -5,6 +5,7 @@ import '../../../gen/assets.gen.dart';
 import '../../../gen/colors.gen.dart';
 import '../../../gen/fonts.gen.dart';
 import '../../../widgets/buttons/button_icon_with_text_shared.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
@@ -21,126 +22,133 @@ class LoginScreen extends ConsumerWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            child: null /* add child content here */,
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
                 padding: const EdgeInsets.only(top: 80),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      height: 65,
-                      width: 65,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: ColorName.blueColor.withOpacity(0.2),
-                          backgroundBlendMode: BlendMode.darken),
-                      child: const Icon(
-                        size: 40,
-                        Icons.photo_library_outlined,
-                        color: ColorName.whiteColor,
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 10),
-                      width: 200,
-                      child: Assets.images.acbone.image(),
-                    ),
-                    Container(
-                      height: 120,
-                      width: 65,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: ColorName.blueColor.withOpacity(0.2),
-                          backgroundBlendMode: BlendMode.darken),
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                            size: 45,
-                            Icons.storefront_outlined,
-                            color: ColorName.whiteColor,
-                          ),
-                          Icon(
-                            size: 45,
-                            Icons.more_horiz_rounded,
-                            color: ColorName.whiteColor,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                child: loginHeaderViewElement(),
               ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 50),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 60,
-                      width: double.infinity,
-                      margin: const EdgeInsets.only(
-                          top: 32, left: 16, right: 16, bottom: 32),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: ColorName.blueColor),
-                      child: const Center(
-                        child: Text(
-                          'ĐĂNG NHẬP',
-                          style: TextStyle(
-                              // fontFamily: FontFamily.raleway,
-                              fontSize: 20,
-                              color: ColorName.whiteColor,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ButtonIconWithTextShared(
-                          height: 30,
-                          width: 30,
-                          icon: const Icon(
-                            Icons.shield_outlined,
-                            color: ColorName.whiteColor,
-                          ),
-                          text: "Lấy OTP",
-                          onPressed: () {},
-                        ),
-                        ButtonIconWithTextShared(
-                          height: 30,
-                          width: 30,
-                          icon: const Icon(
-                            Icons.how_to_reg,
-                            color: ColorName.whiteColor,
-                          ),
-                          text: "Đăng ký",
-                          onPressed: () {},
-                        ),
-                        ButtonIconWithTextShared(
-                          height: 30,
-                          width: 30,
-                          icon: const Icon(
-                            Icons.qr_code_scanner,
-                            color: ColorName.whiteColor,
-                          ),
-                          text: "Mã QR",
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                child: loginFooterViewElement(context),
               ),
             ],
           )
         ],
       ),
+    );
+  }
+
+  Widget loginHeaderViewElement() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          height: 65,
+          width: 65,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: ColorName.blueColor.withOpacity(0.2),
+              backgroundBlendMode: BlendMode.darken),
+          child: const Icon(
+            size: 40,
+            Icons.photo_library_outlined,
+            color: ColorName.whiteColor,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 10),
+          width: 200,
+          child: Assets.images.acbone.image(),
+        ),
+        Container(
+          height: 120,
+          width: 65,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: ColorName.blueColor.withOpacity(0.2),
+              backgroundBlendMode: BlendMode.darken),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                size: 45,
+                Icons.storefront_outlined,
+                color: ColorName.whiteColor,
+              ),
+              Icon(
+                size: 45,
+                Icons.more_horiz_rounded,
+                color: ColorName.whiteColor,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget loginFooterViewElement(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 60,
+          width: double.infinity,
+          margin: const EdgeInsets.only(
+              top: 32, left: 16, right: 16, bottom: 32),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: ColorName.blueColor),
+          child: Center(
+            child: Text(
+              AppLocalizations.of(context)!.helloWorld,
+              style: const TextStyle(
+                // fontFamily: FontFamily.raleway,
+                  fontSize: 20,
+                  color: ColorName.whiteColor,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ButtonIconWithTextShared(
+              height: 30,
+              width: 30,
+              icon: const Icon(
+                Icons.shield_outlined,
+                color: ColorName.whiteColor,
+              ),
+              text: "Lấy OTP",
+              onPressed: () {},
+            ),
+            ButtonIconWithTextShared(
+              height: 30,
+              width: 30,
+              icon: const Icon(
+                Icons.how_to_reg,
+                color: ColorName.whiteColor,
+              ),
+              text: "Đăng ký",
+              onPressed: () {},
+            ),
+            ButtonIconWithTextShared(
+              height: 30,
+              width: 30,
+              icon: const Icon(
+                Icons.qr_code_scanner,
+                color: ColorName.whiteColor,
+              ),
+              text: "Mã QR",
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
